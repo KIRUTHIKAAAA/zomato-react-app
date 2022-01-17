d3074f3f0aa48d6bb11499b0860208dee75ae77e
KIRUTHIKAAAA <73058162+KIRUTHIKAAAA@users.noreply.github.com>
2022-01-18 01:56:22 +0530

    frontend deployed

---
+++ src/layouts/Homepage.layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import FoodTab from '../components/FoodTab/index'
+import Navbar from "../components/Navbar/index";
+
+function HomeLayout({ props, children }) {
+  return (
+    <div>
+      <Navbar {...props} />
+      <FoodTab {...props} />
+      <div className="container mx-auto px-4 lg:px-20">{children}</div>
+    </div>
+  );
+}
+
+export default HomeLayout;
